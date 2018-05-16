c843ce37913e2de0b64467b0967f9e0ca16c2a9e
miguelrioscalle <miguelrioscalle@gmail.com>
2018-05-16 14:23:28 +0200

    Add files via upload

---
+++ Modelos/Implementaci-n en Java/Cliente.java
@@ -0,0 +1,66 @@
+package prCliente;
+
+import java.util.*;
+
+public class Cliente {
+	
+	private String usuario;
+	private String contraseña;
+	private String email;
+	private double reputacion;
+	private List<String> seguidores;
+	private List<String> seguidos;
+
+	public Cliente(String u, String c, String e)
+	{
+		this.usuario = u;
+		this.contraseña = c;
+		this.email = e;
+		this.reputacion = 0.0;
+		seguidores = new LinkedList<>();
+		seguidos = new LinkedList<>();
+	}
+	public String getUsuario()
+	{
+		return this.usuario;
+	}
+	public String getContraseña()
+	{
+		return this.contraseña;
+	}
+	public String getEmail()
+	{
+		return this.email;
+	}
+	public double getReputacion()
+	{
+		return this.reputacion;
+	}
+	public List<String> getSeguidores()
+	{
+		return this.seguidores;
+	}
+	public List<String> getSeguidos()
+	{
+		return this.seguidos;
+	}
+	public void seguirUsuario(Cliente cl)
+	{
+		cl.seguidores.add(this.usuario);
+		this.seguidos.add(cl.usuario);
+	}
+	public void verPerfil()
+	{
+		System.out.println("Nombre de usuario: "+this.usuario+"; E-mail de contacto: "+this.email+"; Reputación: "+this.reputacion
+				+"; Seguidores: "+this.seguidores.size()+"; Seguidos: "+this.seguidos.size()+" .");
+	}
+	public void verOferta()
+	{
+		// pendiente de hacer, primero debe crearse la clase Oferta.
+	}
+	public void compartirOferta()
+	{
+		// pendiente de hacer, primero debe crearse la clase Oferta.
+
+	}
+}
