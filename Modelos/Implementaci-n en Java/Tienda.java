5c1ad4b59ca40df0c5a231821f51ce472eee8475
Albert0espejo <albertowere12@outlook.com>
2018-05-20 13:37:36 +0200

    Add files via upload

---
+++ Modelos/Implementaci-n en Java/Tienda.java
@@ -0,0 +1,10 @@
+
+public class Tienda {
+	private int id_tienda;
+	private String direccion;
+	private String dueño;
+	private Coleccion_oferta ofertas;
+	private String[][] horario = new String[2][7]; // Primera fila mañana, segunda fila tyarde
+	private Coleccion_opinion opiniones;
+
+}
