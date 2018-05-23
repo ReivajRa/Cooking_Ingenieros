d76c9103eff7689ca31a1f878f62dd55557baa1c
ionutT <ionut_uma@hotmail.com>
2018-05-23 12:26:20 +0200

    Creacion de tiendas y clientes en main

--- Proyecto/Cooking Ingenieros/src/Main.java
+++ Proyecto/Cooking Ingenieros/src/Main.java
@@ -7,9 +7,11 @@ public class Main {
 		Cliente cliente1 = new Cliente("Juan","12345","juanito@gmail.com");
 		Cliente cliente2 = new Cliente("Pepe","pepitoperez","pepeguapo@gmail.com");
 		//3 tiendas de prueba
-		Tienda tienda1 = new Tienda();
-		Tienda tienda2 = new Tienda();
-		Tienda tienda3 = new Tienda();
+		Tienda tienda1 = new Tienda("LaTiendaEnCasa", "Calle Ninja Nº 13", "Roberto junior");
+		Tienda tienda2 = new Tienda("El Corteingles","Calle Lolito Fernandez", "Silvia Charro");
+		Tienda tienda3 = new Tienda("Ikea","Avenida El trabuco Nº 4", "Luke Andador del cielo");
+		
+		
 	}
 
 }
