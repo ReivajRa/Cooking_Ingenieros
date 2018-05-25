96ca465fc61e2e5b4a7b57199e7d622d2d011ebb
mariabelenvv <37370581+mariabelenvv@users.noreply.github.com>
2018-05-25 18:10:40 +0200

    Paquete Test

---
+++ Proyecto/Cooking Ingenieros/src/Test/OpinionTest.java
@@ -0,0 +1,24 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OpinionTest {
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOpinion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
