0e8c8ef6190f0f28e4c6c6a28a4df40cc6ad1e7a
miguelangelsomovilla <mianfeso4c@gmail.com>
2018-05-30 22:01:58 +0200

    Add files via upload

---
+++ Proyecto/Cooking Ingenieros/src/Test/ClienteTest.java
@@ -0,0 +1,24 @@
+package CheapDeal;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClienteTest {
+	Cliente c;
+	
+	@Before
+	public void setUp(){
+		c = new Cliente("usuariox","user4576","usuario@ci.com");
+	}
+
+
+	@Test
+	public void testExisteCliente() {
+		assertNotNull("no existe el cliente", c);
+		assertEquals("el usuario esta mal", "usuariox",c.getUsuario());
+		assertEquals("la contraseña esta mal", "user4576", c.getContrasenia());
+		assertEquals("el correo esta mal", "usuario@ci.com",c.getEmail());
+		assertTrue("la reputacion deberia ser 0.0", 0.0==c.getReputacion() );
+	}
+}
