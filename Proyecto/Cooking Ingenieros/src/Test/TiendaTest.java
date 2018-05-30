a72d7b6c9478f1b693a6f978d0977d120e69ff99
Manuel Jurado Pérez <37370457+iManu98@users.noreply.github.com>
2018-05-30 17:18:54 +0200

    Añadir: before, after, y testañadiroferta

--- Proyecto/Cooking Ingenieros/src/Test/TiendaTest.java
+++ Proyecto/Cooking Ingenieros/src/Test/TiendaTest.java
@@ -4,15 +4,12 @@ import CheapDeal.Oferta;
 import CheapDeal.Opinion;
 import CheapDeal.Tienda;
 import static org.junit.Assert.*;
-import java.util.HashSet;
+import java.util.*;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.*;
 
 public class TiendaTest {
+	private Tienta t1;
 	private static int id=1;
 	private int id_tienda;
 	private String nombre;
@@ -23,21 +20,20 @@ public class TiendaTest {
 	private HashSet<Opinion> opiniones;
 	
 	@Rule
-	public ExpectedException esperado = ExpectedException.none();
+	//public ExpectedException esperado = ExpectedException.none();
 	
 	@Before
 	public void setUp() {
-		
+		t1 = new Tienda("TuKolchon","Calle123","Ninja");
 	}
 	
 	@After
 	public void tearDown() {
-		
+		t1 = null;
 	}
 	
 	@Test
 	public void testTienda() {
-		Tienda t1 = new Tienda("TuKolchon", "Calle123", "Ninja");
 		assertEquals("Nombre: TuKolchon",String.valueOf("TuKolchon"),t1.getNombre());
 		assertEquals("Direccion: Calle123",String.valueOf("Calle123"),t1.getDireccion());
 		assertEquals("Duenio: Ninja",String.valueOf("Ninja"),t1.getDuenio());
@@ -46,18 +42,27 @@ public class TiendaTest {
 
 	@Test
 	public void testAnadirOferta() {
-		//String p, double d, Tienda t, double pr, String des, HashSet<Categorias> cat
-		//cat.add(Categorias.ELECTRODOMESTICOS);
-		//Oferta of = new Oferta("Termo mix", 40.00, 2999.00, 
-		//		"Maquina de cocinar del siglo 22 increiblemente util, Doraemon approves", cat);
-		//cat.remove();
-		//ofertas.add(of);
-		//assertNotNull();
+		Map<Integer,Oferta> y;
+		HashSet<Categorias> cat = new HashSet<Categorias>();
+		cat.add(Categorias.ELECTRODOMESTICOS);
+		
+		t1.anadirOferta"Termo mix", 40.00, 2999.00,"Maquina de cocinar del siglo 22 increiblemente util", cat);
+		cat.removeAll(cat);
+		y = t1.getOfertas();
+		assertNotNull("Oferta: Texmo mix es nula",y);
 	}
 
 	@Test
 	public void testQuitarOferta() {
-		fail("Not yet implemented");
+		Map<Integer,Oferta> x;
+		HashSet<Categorias> cat = new HashSet<Categorias>();
+		cat.add(Categorias.ELECTRODOMESTICOS);
+		
+		t1.anadirOferta("Termo mix", 40.00, 2999.00, "Maquina de cocinar del siglo 22 increiblemente util", cat);
+		//t1.quitarOferta("Termo mix", 40.00, 2999.00, "Maquina de cocinar del siglo 22 increiblemete util", cat);
+		cat.removeAll(cat);
+		x = t1.getOfertas();
+		assertNull("Oferta: Termo mix, no es nula", x);
 	}
 
 	@Test
