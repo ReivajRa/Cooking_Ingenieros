1762a37f36ae1dbb9f6ac2534825a959dfcf363f
ionutT <ionut_uma@hotmail.com>
2018-06-01 14:40:52 +0200

    Actualizacion buena de Ordenacion

--- Proyecto/Cooking Ingenieros/src/CheapDeal/Ordenacion.java
+++ Proyecto/Cooking Ingenieros/src/CheapDeal/Ordenacion.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Ordenacion {
 	 public Map<Integer,Tienda> Master;
 	 int filtro;
-	 public Map<Integer,Tienda> OfertasCercanas;
+	 public LinkedList OfertasCercanas;
 	 
 	 public Ordenacion(double X, double Y, Map<Integer,Tienda> tiendas, int filtro) {
 		 Master = new HashMap<Integer, Tienda>(tiendas);
